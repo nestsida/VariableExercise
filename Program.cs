e1d93c401d2e7a6bf73860a24a3a4c7508a322de
sarah sida <nestsida@gmail.com>
2021-09-27 12:24:26 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Variable_exercise
+{
+    class Program
+    {
+        private const decimal V = 6.2m;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string lastName;
+            lastName = "Snow";
+            Console.WriteLine("My last name is Snow.");
+            
+            int myAge = 35;
+            Console.WriteLine("My age is 35.");
+
+            char nameInitial = 'S';
+            Console.WriteLine("My first name initial is S.");
+            bool fullName = true;
+            Console.WriteLine("If you do not tell the truth,your answer is true.");
+            decimal num;
+            Console.WriteLine("if you have no value its num.");
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private static void NewMethod()
+        {
+           
+        }
+    }
+}
